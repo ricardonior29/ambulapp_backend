f6e17ffab1d4933f5338881247b34ce4daf94647
Ricardo Niño <ricardonior@gmail.com>
2019-10-25 00:51:29 -0500

    Create test.js

---
+++ routes/api/test.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+// @route POST api/test/1
+// @desc Login Ambulancia or Centro Medico and return JWT token
+// @access Public
+router.post("/1", (req, res) => {
+  return res.send("on");
+});
+
+module.exports = router;
